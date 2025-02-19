b165c7a0dfb2ea6dfbad6b6c2404524801c5b7a1
jan-niklasml <182075087+jan-niklasml@users.noreply.github.com>
2025-02-19 10:12:29 +0100

    Update update_chart_versions.py

--- update_chart_versions.py
+++ update_chart_versions.py
@@ -8,11 +8,9 @@ def increment_version(version):
     return f"{v.major}.{v.minor}.{v.micro + 1}"
 
 parser = argparse.ArgumentParser(description='Upgrade Versions')
-parser.add_argument('--appVersionBackend', help='New App-Version for backend')
-parser.add_argument('--appVersionFrontend', help='New App-Version for frontend')
-parser.add_argument('--pathToChart', help='Path to superior Chart.yaml')
-parser.add_argument('--pathToBackendChart', help='Path to backend Chart.yaml')
-parser.add_argument('--pathToFrontendChart', help='Path to frontend Chart.yaml')
+parser.add_argument('--appVersion', help='New App-Version')
+parser.add_argument('--pathToChart', help='Path to Chart.yaml')
+parser.add_argument('--pathToParentChart', help='Path to parent Chart.yaml')
 args = parser.parse_args()
 
 yaml = ruamel.yaml.YAML()
@@ -20,26 +18,17 @@ yaml.preserve_quotes = True
 
 with open(args.pathToChart, "r") as file:
     chart_file_data = yaml.load(file)
-with open(args.pathToBackendChart, "r") as file:
-    backend_chart_file_data = yaml.load(file)
-with open(args.pathToFrontendChart, "r") as file:
-    frontend_chart_file_data = yaml.load(file)
+with open(args.pathToParentChart, "r") as file:
+    parent_chart_file_data = yaml.load(file)
 
-if backend_chart_file_data["appVersion"] != args.appVersionBackend:
-    backend_chart_file_data["version"] = increment_version(backend_chart_file_data["version"])
-    backend_chart_file_data["appVersion"] = DoubleQuotedScalarString(args.appVersionBackend)
-    backend_entry_index = chart_file_data["dependencies"].index(next(filter(lambda entry: entry['name'] == 'backend', chart_file_data["dependencies"]), None))
-    chart_file_data["dependencies"][backend_entry_index]["version"] = backend_chart_file_data["version"]
-    
-if frontend_chart_file_data["appVersion"] != args.appVersionFrontend:
-    frontend_chart_file_data["version"] = increment_version(frontend_chart_file_data["version"])
-    frontend_chart_file_data["appVersion"] = DoubleQuotedScalarString(args.appVersionFrontend)
-    frontend_entry_index = chart_file_data["dependencies"].index(next(filter(lambda entry: entry['name'] == 'frontend', chart_file_data["dependencies"]), None))
-    chart_file_data["dependencies"][frontend_entry_index]["version"] = frontend_chart_file_data["version"]
+if chart_file_data["appVersion"] != args.appVersion:
+    chart_file_data["version"] = increment_version(chart_file_data["version"])
+    chart_file_data["appVersion"] = DoubleQuotedScalarString(args.appVersion)
+    entry_index = parent_chart_file_data["dependencies"].index(next(filter(lambda entry: entry['name'] == chart_file_data["name"], parent_chart_file_data["dependencies"]), None))
+    parent_chart_file_data["dependencies"][entry_index]["version"] = chart_file_data["version"]
+    parent_chart_file_data["version"] = increment_version(parent_chart_file_data["version"])
 
 with open(args.pathToChart, "w") as file:
     yaml.dump(chart_file_data, file)
-with open(args.pathToBackendChart, "w") as file:
-    yaml.dump(backend_chart_file_data, file)
-with open(args.pathToFrontendChart, "w") as file:
-    yaml.dump(frontend_chart_file_data, file)
+with open(args.pathToParentChart, "w") as file:
+    yaml.dump(parent_chart_file_data, file)
